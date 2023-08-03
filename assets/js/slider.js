8b23e1db5c5795db84c319743d537b00e63b8831
unknown <xuandai062001@gmail.com>
2023-08-03 16:33:43 +0700

    bla bla

---
+++ assets/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('#owlcarousel-first-slider').owlCarousel({
+        loop: true,
+        margin: 30,
+        dots: true,
+        nav: true,
+        items: 2,
+    });
+
+  });
