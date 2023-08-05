96f2ef013f89c821a944b6a989396375de1ae779
unknown <xuandai062001@gmail.com>
2023-08-05 19:16:13 +0700

    fsdfasf

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
     $(".header").load("header.html"); 
 
+
+
+    // nav animation
+
+
+    setTimeout(
+     function(){
+        var path1 = $('.path1');
+        var path2 = $('.path2');
+        var mline = $('.mline');
+        $('.svgburg').click(function(){
+      
+           path1.toggleClass('cross');
+            path2.toggleClass('cross');
+            mline.toggleClass('hide');
+
+
+
+            $(".navbar").toggleClass("active");
+        })
+     }
+    , 100)
+
+    
+ 
+      
   });
+
+
+
